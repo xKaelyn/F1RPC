cabc35e5b3a7b69a123b928cf34eb7d0b7217140
Kaelyn <wrightricky64@gmail.com>
2023-10-25 21:53:57 +0100

    Connect to Discord and show initial presence

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace F1_23_Discord_RPC
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using F1Sharp;
+using F1Sharp.Packets;
+using NetDiscordRpc;
+using NetDiscordRpc.RPC;
+
+namespace F1_23_Discord_RPC
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var discord = new DiscordRPC("1166791756554178671");
+            var client = new TelemetryClient(20777);
+
+            discord.Initialize();
+
+            client.OnLapDataReceive += Client_OnLapDataReceive;
+            resetStatus(client, discord);
+
+            while (true)
+            {
+
+            }
+        }
+
+        private static void Client_OnLapDataReceive(LapDataPacket packet)
+        {
+
+        }
+
+        private static void resetStatus(TelemetryClient client, DiscordRPC discord)
+        {
+            discord.SetPresence(new RichPresence
+            {
+                State = "In the main menu",
+                Assets = new Assets
+                {
+                    LargeImageKey = "f1_23_logo",
+                    LargeImageText = "F1 23"
+                },
+                Timestamps = new Timestamps(DateTime.UtcNow)
+            });
+            Console.WriteLine($"Updated Discord Status: {discord.CurrentPresence.State}");
         }
     }
 }
